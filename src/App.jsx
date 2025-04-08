43f44c1fcb2a0b4cd39578e5d585f0090821ca9c
Faree09 <FARWASAJJAD09@GMAIL.COM>
2025-04-08 11:40:44 -0400

    app

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,25 @@
-import { Routes, Route } from 'react-router-dom';
-import Navbar from './Components/Navbar';
-import Bio from './pages/Bio';
-import Resume from './pages/Resume';
-import Experience from './pages/Experience';
-import Skills from './pages/Skills';
-import Contact from './pages/Contact';
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Home from "./Components/Sections/Home";
+import About from "./Components/Sections/About";
+import Resume from "./Components/Sections/Resume";
+import Experience from "./Components/Sections/Experience";
+import Skills from "./Components/Sections/Skills";
+import Contact from "./Components/Sections/Contact";
+import Navbar from "./Components/Navbar";
 
 function App() {
   return (
-    <>
+    <Router>
       <Navbar />
       <Routes>
-        <Route path="/" element={<Bio />} />
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
         <Route path="/resume" element={<Resume />} />
         <Route path="/experience" element={<Experience />} />
         <Route path="/skills" element={<Skills />} />
         <Route path="/contact" element={<Contact />} />
       </Routes>
-    </>
+    </Router>
   );
 }
 
